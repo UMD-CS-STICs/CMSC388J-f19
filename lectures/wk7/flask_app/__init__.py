fa983262cda2c93a31488cdb545526d1f5b7ac5a
Yashas Lokesh <yashas.lokesh@gmail.com>
2019-10-11 12:30:49 -0400

    wk7 code
    Signed-off-by: Yashas Lokesh <yashas.lokesh@gmail.com>

--- lectures/wk7/flask_app/__init__.py
+++ lectures/wk7/flask_app/__init__.py
@@ -1,6 +1,36 @@
 from flask import Flask
+from flask_talisman import Talisman
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'\xa4\x0c\x9c3B\xa8a\xc4\x19<z\x00\xc2\xc9\xcd\x14'
+
+# Accept all content coming from this same domain
+csp = {
+    'default-src': '\'self\''
+}
+
+# Accept all content coming from this same domain + another trusted domain
+csp = {
+    'default-src': [
+        '\'self\'',
+        'mywebsite.com'
+    ]
+}
+
+# Accept all content from this domain, accept all images from the web,
+# but only accept video and audio content from the domains
+# listed in 'media-src'.
+# Only accept scripts from script-src
+csp = {
+    'default-src': '\'self\'',
+    'img-src': '*',
+    'media-src': [
+        'audiowebsite.com',
+        'videowebsite.com',
+    ],
+    'script-src': 'trusted-scripts.com'
+}
+
+talisman = Talisman(app, content_security_policy=csp)
 
 from flask_app import routes
--- lectures/wk7/flask_app/routes.py
+++ lectures/wk7/flask_app/routes.py
@@ -1,12 +1,18 @@
-from flask_app import app
+from flask_app import app, talisman
 
 from flask import render_template
+from flask_talisman import ALLOW_FROM
 
 # We don't want index to be embeddable
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Any site can embed this page with an iframe
+# @talisman(frame_options=ALLOW_FROM, frame_options_allow_from='*')
+
+# Only allow trusteddomain.com to embed the about page in an iframe
 @app.route('/about')
+@talisman(frame_options=ALLOW_FROM, frame_options_allow_from='https://trusteddomain.com/')
 def about():
     return render_template('about.html')
